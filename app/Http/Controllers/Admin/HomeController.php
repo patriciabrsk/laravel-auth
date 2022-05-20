25634ef19b448eb1355caad97aad3757c15c8e02
patriciabrsk <patriciabresku@gmail.com>
2022-05-20 10:31:15 +0200

    fix home controller mistake

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __construct() {
-        $this->middleware('auth');
-    }
-
     public function index() {
         return view('admin.home');
     }
